2829a5eff3937b3d3bac4cd912ea7dcb230a19cb
esther-jia <jiafuqing@zafh.com.cn>
2017-12-01 11:26:50 +0800

    Initial commit

---
+++ src/main/java/com/esther/test/HelloGradleTest.java
@@ -0,0 +1,13 @@
+package com.esther.test;
+
+/**
+ * @author esther
+ * 2017-12-01 11:19
+ * $DESCRIPTION}
+ */
+
+public class HelloGradleTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Gradle!");
+    }
+}
